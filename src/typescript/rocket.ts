e78f77b49b1caa635c9756e3955f6aec6d792dde
Joshua Strobl <joshua@stroblindustries.com>
2014-08-07 16:55:08 +0300

    Implemented MutationObserver and Mututation Timer (window.setInterval recursively) to check for Rocket component added to help clear up rocket.core.storedComponents.

--- src/typescript/rocket.ts
+++ src/typescript/rocket.ts
@@ -9,6 +9,66 @@
 
 module rocket {
 
+	// #region Rocket Initialization Function
+
+	export function Init() : void {
+		// #region Watch DOM For Components
+
+		if (MutationObserver !== undefined){ // If MutationObserver is supported by the browser
+			var mutationWatcher = new MutationObserver(
+				function(mutations : Array<MutationRecord>){ // Define mutationHandler as a variable that consists of a function that handles mutationRecords
+					mutations.forEach( // For each mutation that occured
+						function(mutation : MutationRecord){
+							var mutationAddedNodesNum : Number = mutation.addedNodes.length;
+							if (mutationAddedNodesNum > 0){ // If the addedNodes are NOT empty
+								for (var i = 0; i < mutationAddedNodesNum; i++){ // For each node in the mutation.addedNodes
+									var addedNode : Node = mutation[i]; // Get the Node
+									var nodeAsElement : Element = addedNode.childNodes[0].parentElement; // "Convert" the Node to an Element for proper attribute fetching by fetching the childNode[0] parentElement
+
+									var potentialElementId = nodeAsElement.getAttribute("data-rocket-component-id"); // Get the potential Rocket component ID
+
+									if (potentialElementId !== null){ // If the element is a Rocket component
+										delete rocket.core.storedComponents[potentialElementId]["HTMLElement"]; // Ensure the HTMLElement in the storedComponents is deleted
+									}
+								}
+							}
+						}
+					)
+				}
+			);
+
+			var mutationWatcherOptions = { // Define mutationWatcherOptions as the options we'll pass to mutationWatcher.observe()
+				childList : true, // Watch child nodes of the element we are watching
+				attributes : false, // Don't bother to watch for attribute changes
+				characterData : false, // Don't bother to watch character data changes
+			};
+
+			mutationWatcher.observe(document.body, mutationWatcherOptions); // Watch the document body with the options provided.
+		}
+		else{ // If MutationObserver is NOT supported
+			// Use an ol' fashion "timer"
+
+			(function mutationTimer(){
+				window.setTimeout( // Set interval to 10000 (10 seconds) with a timeout, forcing the execution to happen within 10 seconds
+					function(){ // Call this function
+						for (var componentId in Object.keys(rocket.core.storedComponents)){ // Quickly cycle through each storedComponent key (we don't need the sub-objects)
+							if (document.querySelector('*[data-rocket-component="' + componentId + '"]') !== null){ // If the component exists in the DOM
+								delete rocket.core.storedComponents[componentId]["HTMLElement"]; // Ensure the HTMLElement in the storedComponents is deleted
+							}
+						}
+
+						mutationTimer(); // Recursively call setTimeout
+					},
+					10000
+				)
+			})();
+		}
+
+		// #endregion
+	}
+
+	// #endregion
+
 	// #region Meta-function for defining Rocket components
 
 	export function Define(type : string, properties : any) : Object{

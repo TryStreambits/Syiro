96d8c816e981b72172be0ab47137fe5ce48e1806
Joshua Strobl <joshua@stroblindustries.com>
2015-10-03 16:34:02 +0300

    Simplified benchmarking to use a single Perfily instance.

--- tests/design/js/perfily.d.ts
+++ tests/design/js/perfily.d.ts
@@ -1,11 +1,15 @@
 declare class Perfily {
-    name: string;
+    autoclearExpecting: boolean;
     duration: number;
     expecting: any;
-    testFunction: Function;
+    iterations: number;
+    name: string;
     outputIntoDocument: boolean;
+    passed: boolean;
+    testFunction: Function;
     constructor(benchmarkProperties?: Object);
-    SetExpecting(benchmarkExpectedResult: Function): void;
+    SetExpecting(benchmarkExpectedResult: any): void;
+    SetIterations(iterations: number): void;
     SetFunction(benchmarkFunction: Function): void;
     SetName(benchmarkName: string): void;
     Run(): void;
